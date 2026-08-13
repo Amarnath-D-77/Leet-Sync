956af1e3e0bb4dbc0c8c8ed5c7b73e5d393998a2
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-08-13 19:17:58 +0530

    Time: 4 ms (67.79%) | Memory: 156.5 MB (71.17%) - LeetSync

--- 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
+++ 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
@@ -1,3 +1,23 @@
+/*
+ OBSERVATION:
+            instead of toggling every window we just use cnt 
+            to find out when we will need a flip 
+             how do we know if want a toggle or not ?
+                case 1:
+                    nums[i]==0, 
+                              I) if cnt is odd ,then we have flipped the 
+                              current i idx odd number of times, which means it has turned to
+                               1 then nums[i] has become 1 , 
+                              we dont change if it is not equal 
+
+                              II) if cnt is even, then we must toggle this because it is 0
+                case 2:
+                     nums[i]==1,
+                              I) if cnt is odd that means it has toggled to  0 we must change it 
+                              II) if cnt is even that means it has not changed it is still 1 so no 
+                              change                          
+
+*/
 class Solution {
 public:
     int minOperations(vector<int>&nums){
