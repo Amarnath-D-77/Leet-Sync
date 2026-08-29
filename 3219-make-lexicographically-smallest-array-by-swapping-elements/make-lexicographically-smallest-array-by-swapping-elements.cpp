0d3fe15aaa3c98f885e5b6fc6310df186e75c857
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-08-29 13:34:50 +0530

    Time: 132 ms (87.32%) | Memory: 149.4 MB (80.28%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>&nums,int limit){
@@ -12,6 +13,10 @@ public:
       while(i<n){
         j=i+1;
         vector<int>ind;
+        /*extending as long as the condition holds which the elements which we swap 
+         in this particular window j-i+1 we can swap any number with any of the number
+         within the family
+        */ 
         while(j<n && v[j].first-v[j-1].first<=limit){
             j++;
         }
@@ -19,6 +24,15 @@ public:
             ind.push_back(v[idx].second);
         }
         sort(ind.begin(),ind.end());
+        /*
+         original array:
+      idx:  0                     1             2               3            4    
+          [member of family 1][mem of fam2][member of fam1][mem of fam 2][mem of fam 1]
+          here  only idx 0,2,4 can be swapped so it cant be swapped with 1 or 2 
+          so we just try to put the smallest numbers belonging to same family as left 
+          as possible to get the lexico smallest 
+          thats what the below loop does
+        */
         for(int k=0;k<ind.size();k++){
             ans[ind[k]]=v[i+k].first;
         }
