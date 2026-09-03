0a93597c57583bcb68ab9b8f893e5ad388f7f771
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-09-03 21:48:18 +0530

    Time: 3 ms (39.04%) | Memory: 9.2 MB (75.77%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -10,15 +10,11 @@ public:
             if(n-nums[i]>=0){
             ways=ways+rec(dp,n-nums[i],nums);
             }
-            else{
-                break;
-            }
         }
-        dp[n]=ways;
+       dp[n]=ways;
         return dp[n];
     }
     int combinationSum4(vector<int>&nums,int target){
-        sort(nums.begin(),nums.end());
         vector<int>dp(target+1,-1);
         dp[0]=1;
         return rec(dp,target,nums);
