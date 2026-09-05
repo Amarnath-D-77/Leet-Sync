b96968b34dca14d62ac350a05d276bef5ee773d2
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-09-05 14:19:20 +0530

    Time: 3 ms (48.58%) | Memory: 12.4 MB (48.11%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -0,0 +1,25 @@
+class Solution{
+public:
+    int lastStoneWeightII(vector<int>&stones){
+        int n=stones.size();
+        int tot=0;
+        for(int x:stones){
+            tot+=x;
+        }
+        int cap=tot/2;
+        vector<vector<int>>dp(n+1,vector<int>(cap+1,0));  
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=cap;j++){
+                int not_take=dp[i-1][j];
+                int take=0;
+                if(j-stones[i-1]>=0){
+                    take=stones[i-1]+dp[i-1][j-stones[i-1]];
+                }
+                dp[i][j]=max(take,not_take);
+            }
+        }
+        int pile2=tot-dp[n][cap];
+       return pile2-dp[n][cap];
+    }
+};
