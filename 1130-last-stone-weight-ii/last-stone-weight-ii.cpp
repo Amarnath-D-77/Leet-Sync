2aa49daf0045c045d9d9d06fcbb4b8dde69994f3
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-09-05 14:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (42.18%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,3 +1,15 @@
+/*
+OBSERVATION:
+           KEY:
+              no matter in which way smash the stone the answer will always be
+              |S1-S2| where S1 is the sum of piles of stones of one group and 
+              and S2 is another sum of stones
+              
+            we are considering  subset1 and subset 2 as tot/2
+            we try to make pickig of items as close to subset1 sum 
+            so that the  difference between the last two stones is 
+            minimized 
+*/
 class Solution{
 public:
     int lastStoneWeightII(vector<int>&stones){
