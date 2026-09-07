f3f4e7cf4414538697bfb2bf54730c717c49f58d
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-09-07 14:16:44 +0530

    Time: 27 ms (71.12%) | Memory: 44.2 MB (38.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,13 +1,25 @@
 /*
 OBSERVATION:
-           1> if the char matches: 
-                    1>we found the char and i-1 ,j-1 
-                    2> we can search for another char that mathces j so i-1,j
-           1.BASE CASE:
-                   1>if the target reaches 0 then the subsequence is found somewhere in the main string so we return 1 ;
-                   2>else if the main string reaches 0 and the index in target string is not zero then the target subsequence cannot be made so we return 0 
-           2. if we build this bottom up ,we initalize dp[j][0] is 1 because the empty target subseq can be formed from any suffixes of the main string
+           recursion logic:
 
+            i->index for s
+            j->index for t
+          base case:
+                   if the index for reaches 0 for string t ,then subseq is formed
+                   so we put 1 
+        if char dont match:
+                         we keep the j same and look for matching char in the remaining 
+                         string 
+        if char match:
+                    case 1:
+                          we proceed to match j-1 th index to charcters from i-1 to 0 in the main string 
+                    case 2:
+                          we try to match the same j to i-1 charcters in the main 
+                          string because a duplicate may be residing in the main 
+                          string
+
+    Because we are counting distinct subsequences we have to look for the
+    duplicate charcters in the main case so we use two cases                  
 */
 class Solution {
 public:
