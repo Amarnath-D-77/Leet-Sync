9ac7d37c3f1f5b42b64ef985dc9a2a345184d945
Amarnath D <amarnath.dashnamoorthy@gmail.com>
2026-09-11 00:34:27 +0530

    Time: 199 ms (7.35%) | Memory: 190.2 MB (5.05%) - LeetSync

--- 664-strange-printer/strange-printer.cpp
+++ 664-strange-printer/strange-printer.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int rec(vector<vector<vector<int>>>&dp,vector<char>&box,vector<int>ocur,int i,int j,int extra){
+    int rec(vector<vector<int>>&dp,vector<char>&box,vector<int>ocur,int i,int j){
         if(i>j){
             return 0;
         }
-        if(dp[i][j][extra]!=INT_MAX){
-            return dp[i][j][extra];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int ans=1+rec(dp,box,ocur,i+1,j,0);
+        int ans=1+rec(dp,box,ocur,i+1,j);
         for(int k=i+2;k<=j;k++){
          if(box[i]==box[k]){
-            ans=min(ans,rec(dp,box,ocur,i+1,k-1,0)+rec(dp,box,ocur,k,j,0));
+            ans=min(ans,rec(dp,box,ocur,i+1,k-1)+rec(dp,box,ocur,k,j));
          }
         }
-        return dp[i][j][extra]=ans;
+        return dp[i][j]=ans;
     }
     int strangePrinter(string s){
         int n=s.size();
@@ -32,8 +32,8 @@ public:
         }
         box.push_back(s[n-1]);
         ocur.push_back(cnt);
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(n+1,INT_MAX)));
-    int ans=rec(dp,box,ocur,0,box.size()-1,0);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    int ans=rec(dp,box,ocur,0,box.size()-1);
     return ans;
     }
 };
